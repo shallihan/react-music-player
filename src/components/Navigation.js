2b6f4bb0c69c54a9d3ab9adcf7c8e42a0e9546e3
shallihan <smhallihan@gmail.com>
2021-05-31 14:21:23 +0100

    library styling improvements

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMusic } from '@fortawesome/free-solid-svg-icons';
+import { faMusic, faBookOpen } from '@fortawesome/free-solid-svg-icons';
 
 const Navigation = ({ libraryStatus, setLibraryStatus }) => {
     return (
         <nav>
             <h1>Waves</h1>
             <button onClick={() => setLibraryStatus(!libraryStatus)}>
-                Library
-                <FontAwesomeIcon icon={faMusic} />
+                <p>{libraryStatus ? <FontAwesomeIcon icon={faMusic} />:<FontAwesomeIcon icon={faBookOpen} />}</p>
             </button>
         </nav>
     )
